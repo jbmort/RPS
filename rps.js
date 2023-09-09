cea6d988ba435d7dd46a643f1501ecdff0cb253a
Jacob Mortensen <141834498+jbmort@users.noreply.github.com>
2023-09-09 14:19:55 -0500

    Add buttons and improved UI

--- rps.js
+++ rps.js
@@ -11,20 +11,17 @@ if (cs == 3) {return ("scissors");}
 const user = prompt("What is your name?")
 document.getElementById("player").innerHTML = (user + "'s score")
 
-// if enter is hit, triggers button
-let input = document.getElementById('selection');
-input.addEventListener('keypress', function(event)
-{if (event.key === "Enter") { 
-    event.preventDefault();
-    document.getElementById("play").click();}
-})
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
 
-// gathers human and computer responses
-function humanSelection () {
-let sel = document.querySelector('input').value;
-let hs = sel.toLowerCase();
-console.log(hs);
-return hs;
+rock.addEventListener('click', () => {humanSelection("rock"); game()})
+paper.addEventListener('click', () => {humanSelection("paper"); game()})
+scissors.addEventListener('click', () => {humanSelection("scissors"); game()})
+
+// gathers human response
+function humanSelection (entry) {
+humSel = entry;
 }
 
 let humanScore = 0
@@ -37,30 +34,26 @@ function reset() {
     document.getElementById("cScore").innerHTML = compScore; 
     document.getElementById("top").innerHTML = "Make your selection!";
     document.getElementById("result").innerHTML = "Let's go again";
-    document.getElementById("selection").innerHTML = ("");
-
 }
-
+let humsel
 function game() {
     let compSel = compSelect();
-    let humSel = humanSelection();
-   
     if (humanScore < 3 && compScore < 3) {
         if (compSel == "rock" && humSel == "paper" || compSel == "paper" && humSel == "scissors" || compSel == "scissors" && humSel == "rock") {
             document.getElementById("result").innerHTML = "You Win this round!";
             ++humanScore;
             document.getElementById("hScore").innerHTML = humanScore;
-            document.getElementById("selection").innerHTML = ("");
+            
         }
         if (compSel == "rock" && humSel == "scissors" || compSel == "paper" && humSel == "rock" || compSel == "scissors" && humSel == "paper") {
             document.getElementById("result").innerHTML = "The Computer won this round";
             ++compScore;
             document.getElementById("cScore").innerHTML = compScore;  
-            document.getElementById("selection").innerHTML = ("");
+           
         }
         if (humSel !== "rock" && humSel !== "paper" && humSel !== "scissors") {
             document.getElementById("result").innerHTML = "Invalid Response";
-            document.getElementById("selection").innerHTML = ("");
+          
         }
         else if (humSel === compSel) {document.getElementById("result").innerHTML = "Tie!";}
     }
@@ -71,12 +64,8 @@ function game() {
     else if (compScore === 3) {
         document.getElementById("top").innerHTML = "Game Over";
         document.getElementById("result").innerHTML = "The computer kicked your butt!";   
-    
     }
-    
 }
-
-
 
     document.getElementById("hScore").innerHTML = humanScore
     document.getElementById("cScore").innerHTML = compScore
